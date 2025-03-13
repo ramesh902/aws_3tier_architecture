97b53def15d3bd155087df4c46baa0a39ab13a4e
ramesh902 <thirumaniramesh96184@gmail.com>
2025-03-13 11:58:29 +0530

    Update DbConfig.js

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -1,6 +1,6 @@
 module.exports = Object.freeze({
-    DB_HOST : 'YOUR-DATABASE-ENDPOINT.ap-south-1.rds.amazonaws.com',
+    DB_HOST : 'database-1.crqgkgiok8r2.ap-south-1.rds.amazonaws.com',
     DB_USER : 'admin',
-    DB_PWD : 'YourPassword',
+    DB_PWD : 'admin1234',
     DB_DATABASE : 'webappdb'
 });
